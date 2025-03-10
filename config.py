f8ab10622fc1d6bd6850c8c059766a52198c092b
DrHakimmm <272203@student.pwr.edu.pl>
2025-03-10 14:15:10 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+YT_API_KEY = '' # TRZEBA DODAC API KEY !!!
+OPENAI_API_KEY = ''
+
+CENSORED_STR = '#####'
+# SENTIMENT_MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+SENTIMENT_MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
+LIKES_COEFFICIENT = 0.1 # między 0 i 1, jaką częścią wagi są komentarze
+THRESHOLD = 0.33
+debug_flag = True
